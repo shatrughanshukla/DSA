d574ff5066a8a6b074d368852234801cb56784b6
shatrughanshukla <shatrughanshukla00@gmail.com>
2025-09-03 02:49:46 +0530

    DSA Quetions

--- Step3_Arrays/J14_sortColors.java
+++ Step3_Arrays/J14_sortColors.java
@@ -34,6 +34,38 @@
         }
     }
 
+    Optimal Approach: 
+
+    class Solution {
+        public void sortColors(int[] nums) {
+            int n = nums.length;
+            int low = 0;
+            int mid = 0;
+            int high = n-1;
+            while(mid <= high){
+                if(nums[mid] == 0){
+                    swap(nums, mid, low);
+                    low++;
+                    mid++;
+                }
+                else if(nums[mid] == 1){
+                    mid++;
+                }
+                else{
+                    swap(nums, mid, high);
+                    high--;
+                }
+            }
+        }
+
+        private void swap(int[] nums, int i, int j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
+TC: O(n);
+SC: O(1);
 */
 public class J14_sortColors {
     
