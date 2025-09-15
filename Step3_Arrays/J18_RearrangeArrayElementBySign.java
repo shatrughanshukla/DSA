11ae4be75ab17314cb9e2dd696a05f1321c70494
shatrughanshukla <shatrughanshukla00@gmail.com>
2025-09-15 23:09:12 +0530

    J18

---
+++ Step3_Arrays/J18_RearrangeArrayElementBySign.java
@@ -0,0 +1,29 @@
+/*
+https://leetcode.com/problems/rearrange-array-elements-by-sign/description/
+
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int n = nums.length;
+        int[] arr = new int[n];
+        int posI = 0;
+        int negI = 1;
+        for(int i = 0; i < n; i++){
+            if(nums[i] > 0){
+                arr[posI] = nums[i];
+                posI += 2;
+            }
+            else{
+                arr[negI] = nums[i];
+                negI += 2;
+            }
+        }
+        return arr;
+    }
+}
+
+*/
+
+class J18_RearrangeArrayElementBySign {
+
+    
+}
