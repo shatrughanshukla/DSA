f8b2494b71026687e6df8abf55c433886eef3d09
shatrughanshukla <shatrughanshukla00@gmail.com>
2025-10-15 01:33:40 +0530

    J20

--- Step3_Arrays/J20_NextLeader.java
+++ Step3_Arrays/J20_NextLeader.java
@@ -21,4 +21,31 @@ class Solution {
     }
 }
 
+Optimal Solution:
+class Solution {
+    static ArrayList<Integer> leaders(int arr[]) {
+        ArrayList<Integer> tempLeaders = new ArrayList<>();
+        int n = arr.length;
+        int maxii = arr[n - 1];
+        for (int i = n - 1; i >= 0; i--) {
+            if (arr[i] >= maxii) {
+                tempLeaders.add(arr[i]);
+                maxii = arr[i];
+            }
+        }
+        reverse(tempLeaders, 0, tempLeaders.size() - 1);
+        return tempLeaders;
+    }
+
+    private static void reverse(ArrayList<Integer> list, int start, int end) {
+        while (end > start) {
+            int temp = list.get(start);
+            list.set(start, list.get(end));
+            list.set(end, temp);
+            start++;
+            end--;
+        }
+    }
+}
+
 */
