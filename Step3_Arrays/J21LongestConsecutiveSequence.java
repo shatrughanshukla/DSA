53a6daccbdbbec3a4342e723ab7b74dd345f8e21
shatrughanshukla <shatrughanshukla00@gmail.com>
2025-10-16 04:42:43 +0530

    J21

--- Step3_Arrays/J21LongestConsecutiveSequence.java
+++ Step3_Arrays/J21LongestConsecutiveSequence.java
@@ -32,4 +32,34 @@ class Solution {
     }
 }
 
+Optimal Solution:
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        int n = nums.length;
+        if(n == 0) return 0;
+
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums){
+            set.add(num);
+        }
+
+        int longestStreak = 0;
+
+        for(int num : set){
+            if(!set.contains(num - 1)){
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while(set.contains(currentNum + 1)){
+                    currentNum++;
+                    currentStreak++;
+                }
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+        return longestStreak;
+    }
+}
+
 */
