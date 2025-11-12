7fee29cc35f60490297332faeea4a6138f90051d
shatrughanshukla <shatrughanshukla00@gmail.com>
2025-11-12 15:09:35 +0530

    J22

---
+++ Step3_Arrays/J22_SetMatrixZeroes.java
@@ -0,0 +1,47 @@
+/*
+
+https://leetcode.com/problems/set-matrix-zeroes/description/
+https://www.notion.so/22SetMatrixZeroes-292f51bd6dd180b08bb6ff858c1f2681?source=copy_link
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int col0 = matrix[0][0];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    if(j != 0){
+                        matrix[0][j] = 0;
+                    }
+                    else col0 = 0;
+                }
+            }
+        }
+
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(matrix[i][j] != 0){
+                    if(matrix[0][j] == 0 || matrix[i][0] == 0){
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+        
+        if(matrix[0][0] == 0){
+            for(int j = 0; j < n; j++){
+                matrix[0][j] = 0;
+            }
+        }
+        
+        if(col0 == 0){
+            for(int i = 0; i < m; i++){
+                matrix[i][0] = 0;
+            }
+        }  
+    }
+}
+
+*/
